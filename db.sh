cfd61f2ff33d2a19232014ed4b446abc80b9630c
Teerapong Kraiamornchai <teerapong@maqe.com>
2016-06-06 13:41:20 +0700

    Added generated DB connection script.

---
+++ db.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+project="${PWD##*/}"
+project=${project/-/}
+
+dbname=$(docker exec ${project}_db_1 bash -c 'echo $MYSQL_DATABASE')
+dbuser=$(docker exec ${project}_db_1 bash -c 'echo $MYSQL_USER')
+dbpassword=$(docker exec ${project}_db_1 bash -c 'echo $MYSQL_PASSWORD')
+dbport=$(docker port ${project}_db_1 | sed -e 's/[^:]*://g')
+
+if [[ -z $dbuser ]]
+	then
+	dbuser="root"
+fi
+
+if [[ -z $dbpassword ]]
+	then
+	dbpassword=$(docker exec ${project}_db_1 bash -c 'echo $MYSQL_ROOT_PASSWORD')
+fi
+
+cp ./docker/db-template.spf ./docker/project.spf
+
+sed -i "" -e "s/PROJECT/${PWD##*/}/g" \
+	-e "s/DB_NAME/${dbname}/g" \
+	-e "s/DB_USER/${dbuser}/g" \
+	-e "s/DB_PASSWORD/${dbpassword}/g" \
+	-e "s/DB_PORT/${dbport}/g" ./docker/project.spf
+
+open ./docker/project.spf
